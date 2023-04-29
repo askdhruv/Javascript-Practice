2d1e071d97db5ac1eab288ad355e2c8303c8f4be
askdhruv <92171175+askdhruv@users.noreply.github.com>
2023-04-29 20:05:30 +0530

    Update FizzBuzz.js

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,3 +1,5 @@
+//FizzBuzz coding exercise in js
+
 var output = [];
 var count = 1;
 function FizzBuzz(){
@@ -21,3 +23,36 @@ function FizzBuzz(){
     
     console.log(output);
 }
+
+//FuzzBuzz with while loop
+
+
+var output = [];
+var count = 1;
+function FizzBuzz(n){
+
+    while(count<=n){
+
+    if(count%3===0 && count%5===0){
+        output.push("FizzBuzz");
+    }
+    
+    else if(count%3===0){
+        output.push("Fizz");
+    }
+    else if(count%5===0){
+        output.push("Buzz");
+    }
+
+    else
+    {
+        output.push(count);
+    }
+    count++;
+    
+    console.log(output);
+        
+    }
+}
+
+// to run - type FizzBuzz(till number you want);
